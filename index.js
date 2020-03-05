6eaa918b2e059b14d0e29dcc36f9cacc03bbf179
abdumalik2014 <abdumalik2014@gmail.com>
2020-03-05 19:09:31 +0500

    3.1.2 Arithmetic Operators

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// type of operators
-/* 
-Arithmetic
-Assignment
-Comparison
-Logical
-Bitwise
-*/
+let x = 10;
+let y = 3;
+
+console.log(x++);
+//addition operator
+// x+y is an expression: expression is something that gives a value
+// % >> remainder operator
+// ** exponentiation
+
+// ++ Increment
+// ++x >> 11. First increment, then show incremented result
+// x++ >> 11. First show x on the console, then increment
+
+// -- Decrement 
