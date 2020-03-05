adcf4c03d73e540184bc2d18754e7ce07928f51e
abdumalik2014 <abdumalik2014@gmail.com>
2020-03-05 19:43:58 +0500

    3.1.6 Ternary (Conditional) operators

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-//Strict equality (Type + Value)
-console.log(1 === 1);  //T
-console.log('1' === 1); //F 
 
-//Lose equality 
-console.log(1 == 1); //T
-console.log('1' == 1); //T
+//If a customer has more than 1000 points, 
+//they are 'gold' customer, otherwise
+//they are 'silver' customer
 
-// This operator '==' looks to the value of operand on the left side '1' string,
-// so it will automatically convert what we have  on the right side (2nd operand which is number) to string 
-// '1' == 1 >> becomes >> '1' == '1'
+let points = 110;
+//first we start with condition
+//then depending on the points, we set type to gold or silver >> this is ternary operator
+let type = points > 100 ? 'gold' : 'silver';
 
-console.log(true == 1); // T
-// true == 1 >> becomes >> true == true
-
-//Lose equality operator '==' doesnt care about types matching, if the types dont match, it will convert the type of what we have on the right side to the type of what we have on the left side 
-//Most of the time, Strict equality operator should be used
+console.log(type);
