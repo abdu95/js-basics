87f54882638f429a42354da4c666f9dc41cc5289
abdumalik2014 <abdumalik2014@gmail.com>
2020-03-05 19:37:28 +0500

    3.1.5 Equality operators

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-let x = 10;
+//Strict equality (Type + Value)
+console.log(1 === 1);  //T
+console.log('1' === 1); //F 
 
-//The result of an expression that includes a comparison operator is a boolean
+//Lose equality 
+console.log(1 == 1); //T
+console.log('1' == 1); //T
 
-//Relational operators
-console.log(x > y);
-console.log(x >= y);
-console.log(x < y);
-console.log(x <= y);
+// This operator '==' looks to the value of operand on the left side '1' string,
+// so it will automatically convert what we have  on the right side (2nd operand which is number) to string 
+// '1' == 1 >> becomes >> '1' == '1'
 
-//Equality operators
-console.log(x === 1);
-console.log(x !== 1);
+console.log(true == 1); // T
+// true == 1 >> becomes >> true == true
+
+//Lose equality operator '==' doesnt care about types matching, if the types dont match, it will convert the type of what we have on the right side to the type of what we have on the left side 
+//Most of the time, Strict equality operator should be used
